1f193f79722ac22dcf9831b63f82b5c5105cd75c
rahul-yadav7 <rky9582@gmail.com>
2023-10-23 15:34:49 +0530

    graph

---
+++ graph/5-329.LongestIncreasingPathinaMatrix.js
@@ -0,0 +1,51 @@
+/**
+ * Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+Output: 4
+ */
+
+let matrix =[[0,1,2,3,4,5,6,7,8,9],[19,18,17,16,15,14,13,12,11,10],[20,21,22,23,24,25,26,27,28,29],[39,38,37,36,35,34,33,32,31,30],[40,41,42,43,44,45,46,47,48,49],[59,58,57,56,55,54,53,52,51,50],[60,61,62,63,64,65,66,67,68,69],[79,78,77,76,75,74,73,72,71,70],[80,81,82,83,84,85,86,87,88,89],[99,98,97,96,95,94,93,92,91,90],[100,101,102,103,104,105,106,107,108,109],[119,118,117,116,115,114,113,112,111,110],[120,121,122,123,124,125,126,127,128,129],[139,138,137,136,135,134,133,132,131,130],[0,0,0,0,0,0,0,0,0,0]]
+// Output: 4
+
+var longestIncreasingPath = function(matrix) {
+    let row = matrix.length;
+    let col = matrix[0].length;
+    let poCount = {};
+    let maxCount = 0
+    let matrixpath;
+    function dsf(i,j,p = null, count = 0, path=[]){
+        if(i<0 ||j<0 || i>= row || j>= col) {
+            return
+        }
+        if(p == null || matrix[i][j]> p) {
+            path.push(matrix[i][j])
+           if(poCount[`${i}${j}`]) {
+               count += poCount[`${i}${j}`]
+               if(count> maxCount) {
+                matrixpath = path
+               }
+               maxCount = Math.max(count, maxCount);
+               return
+           } else {
+               count +=1 
+               if(count> maxCount) {
+                matrixpath = path
+               }
+               maxCount = Math.max(count, maxCount);
+               dsf(i+1,j,matrix[i][j], count, [...path])
+               dsf(i,j+1,matrix[i][j], count,  [...path])
+               dsf(i,j-1,matrix[i][j], count,  [...path])
+               dsf(i-1,j,matrix[i][j], count,  [...path])
+           }
+        }
+        
+
+    }
+    for(let i=0;i<matrix.length; i++) {
+        for(let j=0; j<matrix[i].length; j++) {
+            dsf(i,j);
+        }
+    }
+    console.log(matrixpath)
+    return maxCount
+};
+console.log("🚀 ~ fisingPath:", longestIncreasingPath(matrix))
